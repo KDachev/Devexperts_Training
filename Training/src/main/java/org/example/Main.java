a2ce4455884a6205e2f45b02a2fe5ec70877474f
Kalin Dachev <kdachev@devexperts.com>
2022-12-08 15:09:39 +0200

    Added blocking if Thread calls queue.get() and there are no values

--- Training/src/main/java/org/example/Main.java
+++ Training/src/main/java/org/example/Main.java
@@ -1,21 +1,42 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.example.model.UniqueEventsQueue;
 
+
 public class Main {
+    private static Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         UniqueEventsQueue<Integer> ueq = new UniqueEventsQueue<>();
 
-        System.out.println("Get first element of empty queue: " + ueq.get());
-        ueq.add(1);
-        ueq.add(1);
-        ueq.add(1);
-        ueq.add(1);
-        ueq.add(2);
-        ueq.add(3);
-        System.out.printf("Adding elements 1, 2, 3: \n%s\n", ueq);
-        System.out.println("Get first element after adding elements: " + ueq.get());
-        System.out.println(ueq);
+        Thread thread1 = new Thread(() -> {
+            System.out.println("Get first element of empty queue: ");
+            try {
+                System.out.println("Thread 1 pulled a value from the queue: " + ueq.get());
+            } catch (InterruptedException e) {
+                logger.warn("A thread was interrupted with message: " + e.getMessage());
+            }
+        });
 
+        Thread thread2 = new Thread(() -> {
+            try {
+                Thread.sleep(5000);
+                System.out.println("Adding element 1 to queue" );
+                ueq.add(1);
+                Thread.sleep(200);
+                System.out.println("Adding element 2 to queue" );
+                ueq.add(2);
+                Thread.sleep(200);
+                System.out.println("Adding element 3 to queue" );
+                ueq.add(3);
+            } catch (InterruptedException e){
+                logger.warn("A thread was interrupted with message: " + e.getMessage());
+            }
+        });
+
+        thread1.start();
+        thread2.start();
     }
 }
--- Training/src/main/java/org/example/model/UniqueEventsQueue.java
+++ Training/src/main/java/org/example/model/UniqueEventsQueue.java
@@ -6,24 +6,35 @@ import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class UniqueEventsQueue<T> {
-    private Queue<T> uniqueEventsQueue;
-    private Map<Integer, T> uniqueElementsMap;
+    private final Queue<T> uniqueEventsQueue;
+    private final Map<Integer, T> uniqueElementsMap;
+    private final Object getLock = new Object();
+    private final Object addLock = new Object();
 
     public UniqueEventsQueue(){
         uniqueEventsQueue = new ConcurrentLinkedQueue<>();
         uniqueElementsMap = new HashMap<>();
     }
 
-    public synchronized void add(T element){
-        if (!uniqueElementsMap.containsKey(element.hashCode())) {
-            uniqueElementsMap.put(element.hashCode(), element);
-            uniqueEventsQueue.add(element);
+    public void add(T element){
+        synchronized (uniqueEventsQueue) {
+            if (!uniqueElementsMap.containsKey(element.hashCode())) {
+                uniqueElementsMap.put(element.hashCode(), element);
+                uniqueEventsQueue.add(element);
+                uniqueEventsQueue.notify();
+            }
         }
     }
 
-    public T get (){
-        return uniqueEventsQueue.poll();
-    }
+    public T get () throws InterruptedException {
+        synchronized(uniqueEventsQueue) {
+            if (uniqueEventsQueue.isEmpty()) {
+                System.out.println("Thread goes to sleep");
+                uniqueEventsQueue.wait();
+            }
+            return uniqueEventsQueue.poll();
+        }
+}
 
     @Override
     public String toString() {
